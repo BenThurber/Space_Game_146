ff1077a48b2abcf5061affdeb1374331c2240b63
Ben Thurber <bgt26@uclive.ac.nz>
2019-05-02 23:19:45 +1200

    Added attributes to CrewMember

--- SpaceGame/src/Environment/CrewMember.java
+++ SpaceGame/src/Environment/CrewMember.java
@@ -1,5 +1,26 @@
 package Environment;
 
 public class CrewMember {
-
+	
+	// Should attributes representing percentages be ints 0...100 or floats 0.0...1.0
+	private int health = 100;
+	private int hunger = 0;
+	private int stamina = 100;
+	private String name;  // Should crew members have individual names?
+	
+	/**Crew member sleeps and regains some amount of stamina (health?)*/
+	public void sleep() {
+		
+	}
+	
+	/**Crew member eats and decreases some amount of hunger.*/
+	public void eat() {
+		
+	}
+	
+	/**Crew member eats and regains some amount of health.*/
+	public void useMedicalItem() {
+		
+	}
+	
 }
