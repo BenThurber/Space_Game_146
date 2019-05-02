a814a30d5b2d320ef8c581d4e8f2decd1cf260c8
Ben Thurber <bgt26@uclive.ac.nz>
2019-05-02 23:20:55 +1200

    Added an ArrayList to Crew to hold the members

--- SpaceGame/src/Environment/Crew.java
+++ SpaceGame/src/Environment/Crew.java
@@ -1,4 +1,10 @@
 package Environment;
 
+import java.util.ArrayList;
+
+/**Container class for CrewMember objects.*/
 public class Crew {
+	
+	// ArrayList to store crew members
+	private ArrayList<CrewMember> members = new ArrayList<CrewMember>();
 }
