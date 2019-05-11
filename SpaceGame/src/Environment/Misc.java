cf57cc0b33c85459711f7aa0eab3b31469bd7f51
Ben Thurber <bgt26@uclive.ac.nz>
2019-05-11 20:31:22 +1200

    added Misc class containing miscilaneous static methods

---
+++ SpaceGame/src/Environment/Misc.java
@@ -0,0 +1,11 @@
+package Environment;
+
+public class Misc {
+	public static String capitalize(String s) {
+		if (s.length() > 0) {
+			return Character.toUpperCase(s.charAt(0)) + s.substring(1).toLowerCase();
+		} else {
+			return "";
+		}
+	}
+}
